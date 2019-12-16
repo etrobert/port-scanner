b2083cadcdac6a6c1d8cda859c1e7b86a440be98
erobert <etiennerobert33@gmail.com>
2019-12-16 03:40:13 +0100

    auto format

--- port-scanner.py
+++ port-scanner.py
@@ -2,6 +2,7 @@ import subprocess
 import sys
 import os
 
+
 def new_filename(pattern):
     """Returns the first filename matching pattern for which a file does not exist
     :param pattern: a pattern of the form *%s*
@@ -11,12 +12,13 @@ def new_filename(pattern):
         i += 1
     return pattern % i
 
+
 if __name__ == "__main__":
     tmp_filename = new_filename(".scan%s.xml")
-    returncode = subprocess.call(["nmap", "-v", "-sV", "-oX", tmp_filename] + sys.argv[1:])
+    returncode = subprocess.call(
+        ["nmap", "-v", "-sV", "-oX", tmp_filename] + sys.argv[1:])
     if returncode:
         sys.exit()
     html_filename = new_filename("scan%s.html")
     subprocess.call(["xsltproc", tmp_filename, "-o", html_filename])
     os.remove(tmp_filename)
-
