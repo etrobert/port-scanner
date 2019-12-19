05820bc809b78d278889e317b84083912f5b8e1d
erobert <etiennerobert33@gmail.com>
2019-12-19 23:32:03 +0100

    add ip and target tests

--- test/test_parse_args.py
+++ test/test_parse_args.py
@@ -1,4 +1,4 @@
-from parse_args import is_valid_hostname
+from parse_args import is_valid_hostname, is_valid_ip, is_valid_target
 
 def test_is_valid_hostname_label_length():
   label_63 = "uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu"
@@ -36,3 +36,34 @@ def test_is_valid_hostname():
   assert is_valid_hostname("google.com")
   assert is_valid_hostname("www.google.com")
   assert not is_valid_hostname("http://www.google.com")
+
+def test_is_valid_ip():
+  assert is_valid_ip("0.0.0.0")
+  assert is_valid_ip("127.0.0.1")
+  assert is_valid_ip("192.168.0.1")
+  assert is_valid_ip("255.255.255.255")
+  assert not is_valid_ip("")
+  assert not is_valid_ip("hello")
+  assert not is_valid_ip("www.google.com")
+  assert not is_valid_ip("256.0.0.0")
+  assert not is_valid_ip("999.999.999.999")
+  assert not is_valid_ip("256")
+  assert not is_valid_ip("0.0.0")
+  assert not is_valid_ip("0.0.0.")
+  assert not is_valid_ip("0.0.0.0.")
+  assert not is_valid_ip("0.0.0.0.0")
+
+def test_is_valid_target():
+  assert is_valid_target("www.google.com")
+  assert not is_valid_target("http://www.google.com")
+  assert not is_valid_target("Id love myself some turkey")
+  assert is_valid_target("0.0.0.0")
+  assert is_valid_target("localhost")
+  assert is_valid_target("0.0.0.0/0")
+  assert is_valid_target("0.0.0.0/8")
+  assert is_valid_target("0.0.0.0/16")
+  assert is_valid_target("0.0.0.0/32")
+  assert not is_valid_target("0.0.0.0/33")
+  assert not is_valid_target("0.0.0.0/0/0")
+  assert is_valid_target("localhost/15")
+  assert not is_valid_target("localhost/99")
