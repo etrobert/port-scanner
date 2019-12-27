855124681a5e5d308a4d612a371ada4b611bfb2a
erobert <etiennerobert33@gmail.com>
2019-12-28 00:02:35 +0100

    add httpserver integration test

--- test/test_netns.py
+++ test/test_netns.py
@@ -6,6 +6,12 @@ from toolbox import assert_exit
 from bs4 import BeautifulSoup
 
 try:
+    import http.server as BaseHTTPServer
+except:
+    import BaseHTTPServer
+import threading
+
+try:
     import html  # Python3
 except ImportError:
     import HTMLParser  # Python2
@@ -52,3 +58,10 @@ def test_netns():
     with NetNSSandbox():
         assert_exit(["port_scanner", "localhost", "-o", test_filename], 0)
         assert_open_count(test_filename, 0)
+
+        webserver = BaseHTTPServer.HTTPServer(
+            ('', 8000), BaseHTTPServer.BaseHTTPRequestHandler)
+        threading.Thread(target=webserver.serve_forever).start()
+        assert_exit(["port_scanner", "localhost", "-o", test_filename], 0)
+        webserver.shutdown()
+        assert_open_count(test_filename, 1)
